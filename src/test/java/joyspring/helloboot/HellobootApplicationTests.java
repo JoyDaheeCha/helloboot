5a6cfa8968e7c5e3c4f4d46cf3dbfc64329854e0
daheecha0612 <dahee.cha@gmail.com>
2024-04-01 09:57:37 +0900

    init

---
+++ src/test/java/joyspring/helloboot/HellobootApplicationTests.java
@@ -0,0 +1,13 @@
+package joyspring.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
