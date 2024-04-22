899360e3f810886373893e6fa36eb08040c0b3a6
daheecha0612 <dahee.cha@gmail.com>
2024-04-22 21:47:22 +0900

    서블릿 등록

--- src/main/java/joyspring/helloboot/HellobootApplication.java
+++ src/main/java/joyspring/helloboot/HellobootApplication.java
@@ -1,14 +1,33 @@
 package joyspring.helloboot;
 
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import java.io.IOException;
 
 public class HellobootApplication {
     public static void main(String[] args) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().println("Hello Servlet");
+
+                }
+            }).addMapping("/hello");
+        }); // 서블릿을 서블릿 컨테이너에 추가해준다.
         webServer.start();
     }
 }
